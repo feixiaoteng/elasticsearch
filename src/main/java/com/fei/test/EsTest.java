3d98dd58e7285099dadc3e76dd08baa1e120a990
tengfei.dai <fxt17693636789>
2020-03-11 15:36:49 +0800

    初始提交

---
+++ src/main/java/com/fei/test/EsTest.java
@@ -0,0 +1,13 @@
+package com.fei.test;
+
+/**
+ * @ProjectName: elasticsearch
+ * @Package: com.fei.test
+ * @ClassName: EsTest
+ * @Author: feixiaoteng
+ * @Date: 2020-03-11 15:36
+ * @Version: 1.0.0
+ * @Description: 测试好不好使
+ */
+public class EsTest {
+}
